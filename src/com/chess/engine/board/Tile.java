df90acaf7b8b589bd9dd39e0180225bdf8caafc2
vxibh <vxx6hxv@gmail.com>
2023-12-17 18:06:14 +0530

    Defined Piece Class & Other Fixes

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -7,20 +7,22 @@ import java.util.Map;
 public abstract class Tile {
     protected final int tileCoordinate; //can only be accessed by subclasses
 
-    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer,EmptyTile> createAllPossibleEmptyTiles() {
+
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
         for(int i=0; i<64; i++)
         {
             emptyTileMap.put(i, new EmptyTile(i));
         }
 
+        //Collections.unmodifiableMap(emptyTileMap);
         return ImmutableMap.copyOf(emptyTileMap);
     }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece) {
-        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
     //tile constructor
     private Tile(int tileCoordinate){
@@ -34,7 +36,7 @@ public abstract class Tile {
 
     //empty tile method
     public static final class EmptyTile  extends Tile {
-        EmptyTile(final int coordinate) {
+        private EmptyTile(final int coordinate) {
             super(coordinate);
         }
 
@@ -52,9 +54,9 @@ public abstract class Tile {
     public static final class OccupiedTile extends Tile {
 
         private final Piece pieceOnTile; //can't be referenced outside this unless getPiece is called
-        OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+        private OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
             super(tileCoordinate);
-            this.pieceOnTile=pieceOnTile;
+            this.pieceOnTile = pieceOnTile;
         }
 
         @Override
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,5 +1,18 @@
 package com.chess.engine.pieces;
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import java.util.List;
 
-public class Piece {
+public abstract class Piece {
+    protected final int piecePosition; //method for piece position
+    protected final Alliance pieceAlliance; //method for class
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+        this.pieceAlliance = pieceAlliance;
+        this.piecePosition = piecePosition;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board); //returning a list of legal moves
 
 }
