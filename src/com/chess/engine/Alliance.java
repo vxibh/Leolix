a0a978585855cffd14190c0094c5d5e1733c0be2
vxibh <vxx6hxv@gmail.com>
2023-12-18 18:49:03 +0530

    Basic Pawn Move

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -1,6 +1,19 @@
 package com.chess.engine;
 
 public enum Alliance {  //using enum bcoz its typesafe and less constant instances
-    WHITE,
-    BLACK
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    };
+
+    public abstract int getDirection();
+
 }
